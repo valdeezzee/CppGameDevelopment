e683ae059c612813c423ca2b7254c7da4cef5e35
valdeezzee <jvalderrama91@hotmail.com>
2016-07-22 15:25:05 -0400

    Begin chapter 5 now.

--- CppGameDevelopment/CppGameDevelopment.cpp
+++ CppGameDevelopment/CppGameDevelopment.cpp
@@ -7,6 +7,7 @@
 #include <algorithm>
 #include <ctime>
 #include <cstdlib>
+#include <cctype>
 
 
 using namespace std;
@@ -188,6 +189,7 @@ int main()
 	*/
 
 	//Using algorithms
+	/*
 	vector<int>::const_iterator iter;
 
 	cout << "Creating a list of score.";
@@ -233,7 +235,91 @@ int main()
 	{
 		cout << *iter << endl;
 	}
-
+	*/
 	
+
+	//Memory allocation for vectors
+	//vectors capacity increases when the size is increased. about double capacity
+	/*
+	cout << "Creating a 10 element vector to hold score.\n";
+	vector<int> scores(10, 0);
+	cout << "Vector size is :" << scores.size() << endl;
+	cout << "Vector capacity is:" << scores.capacity() << endl;
+	cout << "Adding a score.\n";
+	scores.push_back(0);
+	cout << "Vector size is :" << scores.size() << endl;
+	cout << "Vector capacity is:" << scores.capacity() << endl;
+	//reserve() gives you contorol over when a reallocation of additional memory occurs
+	//ex: scores.reserve(20); reserves memory for 10 additional elements
+	*/
+
+	//Hangman
+	const int MAX_WRONG = 8;
+	vector<string> words;
+	words.push_back("GUESS");
+	words.push_back("HANGMAN");
+	words.push_back("DIFFICULT");
+
+	srand(static_cast<unsigned int> (time(0)));
+	random_shuffle(words.begin(), words.end());
+	const string THE_WORD = words[0];
+	int wrong = 0;
+	string soFar(THE_WORD.size(), '-');
+	string used = "";
+
+	cout << "Welcome to Hangman. Good luck!\n";
+
+	while ((wrong < MAX_WRONG) && (soFar != THE_WORD))
+	{
+		cout << "\n\nYou have " << (MAX_WRONG - wrong);
+		cout << " incorrect guesses left.\n";
+		cout << "\nYou've used the following letters:\n" << used << endl;
+		cout << "\nSo far, the word is:\n" << soFar << endl;
+
+		char guess;
+		cout << "\n\nEnter your guess: ";
+		cin >> guess;
+		guess = toupper(guess);//makes uppercase since secret word is in uppercase
+		//string::npos not found. so while guess is not found, continue while statement
+		while (used.find(guess) != string::npos)
+		{
+			cout << "\nYou've already guessed " << guess << endl;
+			cout << "Enter your guess: ";
+			cin >> guess;
+			guess = toupper(guess);
+		}
+
+		used += guess;
+
+		if (THE_WORD.find(guess) != string::npos)
+		{
+			cout << "That's right! " << guess << " is in the word.\n";
+
+			for (int i = 0; i < THE_WORD.length(); i++)
+			{
+				if (THE_WORD[i] == guess)
+				{
+					soFar[i] = guess;
+				}
+			}
+		}
+		else
+		{
+			cout << "Sorry, " << guess << " isn't in the word.\n";
+			wrong++;
+		}		
+	}
+
+	if (wrong == MAX_WRONG)
+	{
+		cout << "\nYou've been hanged!";
+	}
+	else
+	{
+		cout << "\nYou guessed it!";
+	}
+
+	cout << "\nThe word was " << THE_WORD << endl;
+
 }
 
